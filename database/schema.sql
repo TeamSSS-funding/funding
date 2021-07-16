15b141aacac7aa101e9790cc3f275350ff108d9f
singy970 <gkstmdgml423@naver.com>
2021-07-16 19:38:00 +0900

    [#33] member 테이블 변경

--- database/schema.sql
+++ database/schema.sql
@@ -1,11 +1,14 @@
-create table MEMBER
+create table member
 (
-    M_ID VARCHAR(255) not null,
-    M_PASSWORD VARCHAR(50),
-    M_NAME VARCHAR(255),
-    M_PHONE VARCHAR(255),
-    M_EMAIL VARCHAR(500),
-    M_TYPE VARCHAR(255),
-    ENABLED bit default 1,
-    AUTHORITY VARCHAR(255) default 'ROLE_MEMBER'
+    M_ID varchar(255) not null,
+    M_PASSWORD varchar(255) null,
+    M_NAME varchar(255) null,
+    M_PHONE varchar(255) null,
+    M_EMAIL varchar(500) null,
+    M_TYPE varchar(255) null,
+    ENABLED bit default b'1' null,
+    AUTHORITY varchar(255) default 'ROLE_MEMBER' null,
+    CITY varchar(255) null,
+    STREET varchar(255) null,
+    ZIPCODE varchar(255) null
 );
