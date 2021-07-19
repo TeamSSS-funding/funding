45be0d03f609c131b2e5c71dc3e6c3f6c7954654
Seulah-coder <rser@hanmail.net>
2021-07-19 20:56:15 +0900

    [13]security에서 id불러오기

--- src/main/java/io/github/mygoodsupporter/controller/ProjectController.java
+++ src/main/java/io/github/mygoodsupporter/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package io.github.mygoodsupporter.controller;
 
+import io.github.mygoodsupporter.dao.MemberDAO;
 import io.github.mygoodsupporter.domain.Project;
 import io.github.mygoodsupporter.security.MemberDetails;
 import io.github.mygoodsupporter.service.ProjectService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,16 +23,18 @@ public class ProjectController {
 
     @Autowired
     private ProjectService ps;
-
+    @Autowired
+    private MemberDAO memberDAO;
     private ModelAndView mav;
 
     //로그인 아이디 불러오기
     @GetMapping("/principal")
     @ResponseBody
-    public String currentUserNameByPrincipal(Principal principal) {
-        MemberDetails memberDetails = (MemberDetails) principal;
-        log.info("Member[id=" + memberDetails.getId() + ", " + memberDetails.getUsername() + "]");
-        return memberDetails.getId();
+    public String currentUserNameByPrincipal(Principal principal, @AuthenticationPrincipal MemberDetails memberDetails) {
+        log.debug("MemberDetails:" + memberDetails.getId() + " " + memberDetails.getUsername() + " " + memberDetails.getEmail());
+        log.debug(principal.getName());
+
+        return principal.getName() + "MemberDetails:" + memberDetails.getId() + " " + memberDetails.getUsername() + " " + memberDetails.getEmail();
     }
 
     // 프로젝트 신청 화면 요청
@@ -42,14 +46,14 @@ public class ProjectController {
     }
 
     //프로젝트 신청페이지
-    @RequestMapping(value="projectRequest")
+    @RequestMapping(value="/projectRequest")
     public ModelAndView projectRequest(@ModelAttribute Project pdto) throws IllegalStateException, IOException {
         mav = ps.projectRequest(pdto);
         return mav;
     }
 
     //프로젝트 심사 페이지
-    @RequestMapping(value="projectSimsa")
+    @RequestMapping(value="/projectSimsa")
     public ModelAndView projectSimsa(@ModelAttribute Project pdto) {
         mav = ps.projectSimsa(pdto);
         return mav;
