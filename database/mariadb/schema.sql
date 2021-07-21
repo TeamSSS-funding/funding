dc58f8c8377618753bbd399d5e44a28a1aa2d254
safecorners <safeconers.mobile@gmail.com>
2021-07-21 18:14:47 +0900

    [#45][UC01] project ddl 수정

--- database/mariadb/schema.sql
+++ database/mariadb/schema.sql
@@ -1,6 +1,6 @@
 create table member
 (
-    m_id       varchar(255)     not null,
+    m_id       varchar(255)     PRIMARY KEY not null,
     m_password varchar(255)     not null,
     m_name     varchar(255)     not null,
     m_phone    varchar(255)     not null,
@@ -22,4 +22,23 @@ CREATE TABLE proposal
     description   VARCHAR(255) NOT NULL,
     target_amount INT(11)      NOT NULL,
     status        VARCHAR(255) NOT NULL
+);
+
+CREATE SEQUENCE project_seq
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE;
+
+create table project
+(
+    id int(10) primary key not null,
+    member_id varchar(255) not null,
+    name varchar(255) not null,
+    target_amount int(10) not null,
+    current_amount int(10) not null,
+    content varchar(255) not null,
+    start_date date,
+    end_date date,
+    constraint project_member_m_id_fk
+        foreign key (member_id) references member (m_id)
 );
