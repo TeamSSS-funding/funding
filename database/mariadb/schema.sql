f06f5e078924d75ad3c187c8d61fdbd7c1b69413
safecorners <safeconers.mobile@gmail.com>
2021-07-21 13:06:25 +0900

    [#47][UC02] Proposal 테이블 생성

--- database/mariadb/schema.sql
+++ database/mariadb/schema.sql
@@ -14,3 +14,12 @@ create table authority
     authority varchar(255)
 );
 
+CREATE TABLE proposal
+(
+    id            BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    member_id     VARCHAR(255) NOT NULL,
+    title         VARCHAR(255) NOT NULL,
+    description   VARCHAR(255) NOT NULL,
+    target_amount INT(11)      NOT NULL,
+    status        VARCHAR(255) NOT NULL
+);
