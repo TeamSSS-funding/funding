c8cb167d141784097e3dcef4db3edd7ed0d2b970
safecorners <safeconers.mobile@gmail.com>
2021-07-22 11:43:54 +0900

    [#42] memberDetailsService 추가

--- src/main/java/io/github/mygoodsupporter/security/SecurityConfig.java
+++ src/main/java/io/github/mygoodsupporter/security/SecurityConfig.java
@@ -4,18 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MemberDetailsService memberDetailsService;
+
+    public SecurityConfig(MemberDetailsService memberDetailsService) {
+        this.memberDetailsService = memberDetailsService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()
                 .and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
