5e2c886f222cb8f543b3c1f251d2e12c06f8de13
tnqls9582 <tnqls9582@naver.com>
2021-07-22 13:48:38 +0900

    Login 무한루프 오류 해결

--- src/main/java/io/github/mygoodsupporter/security/MemberDetailsService.java
+++ src/main/java/io/github/mygoodsupporter/security/MemberDetailsService.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class MemberDetailsService implements UserDetailsService {
 
     @Autowired
