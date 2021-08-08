4f570f061f262560d23d052975e24d317a98bf45
safecorners <safeconers.mobile@gmail.com>
2021-08-08 13:18:26 +0900

    [#98] Reward: ItemService 추가 리워드의 아이템을 등록하는 서비스입니다.
    Signed-off-by: safecorners <safeconers.mobile@gmail.com>

---
+++ src/main/java/io/github/mygoodsupporter/service/project/ItemService.java
@@ -0,0 +1,35 @@
+package io.github.mygoodsupporter.service.project;
+
+import io.github.mygoodsupporter.domain.project.Item;
+import io.github.mygoodsupporter.mapper.ItemMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemMapper itemMapper;
+
+    public List<Item> getItemsByProjectId(Long projectId) {
+        return itemMapper.getItemsByProjectId(projectId);
+    }
+
+    public Item getItemById(Long itemId) {
+        return itemMapper.getItemById(itemId);
+    }
+
+    public void createItem(Item item) {
+        itemMapper.insertItem(item);
+    }
+
+    public void updateItem(Item item) {
+        itemMapper.updateItem(item);
+    }
+
+    public void deleteItem(Long itemId) {
+        itemMapper.deleteItem(itemId);
+    }
+}
