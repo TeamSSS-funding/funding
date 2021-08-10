3122608081ca7dad3dc0ddc1034b959f4ff149d6
singy970 <gkstmdgml423@naver.com>
2021-08-10 20:34:55 +0900

    [#36] 프로젝트 이미지 수정 기능 및 삭제 기능 구현
    이미지 수정을 위하여 url을 분리하고 합치는 작업을 함
    이미지를 수정하지 않았을때 빈 파일이 입력되지 않도록 파일 사이즈 검사함

--- database/mariadb/schema.sql
+++ database/mariadb/schema.sql
@@ -15,16 +15,6 @@ create table authority
     authority varchar(255)
 );
 
-CREATE TABLE proposal
-(
-    id            BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-    member_id     VARCHAR(255) NOT NULL,
-    title         VARCHAR(255) NOT NULL,
-    description   VARCHAR(255) NOT NULL,
-    target_amount INT(11)      NOT NULL,
-    status        VARCHAR(255) NOT NULL
-);
-
 CREATE SEQUENCE project_seq
     START WITH 1
     INCREMENT BY 1
